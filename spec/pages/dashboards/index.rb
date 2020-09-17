9c21477eba3a5f50adabc5c5131c7b0da5bc2aff
Adam Rice <development@hashnotadam.com>
2020-09-17 15:27:22 +1000

    Remove sign-in fields from Pages::Dashboards::Index
    This should have been done in an earlier step. It appears that, when
    following my own instructions, I skipped a step and left this in the
    file

--- spec/pages/dashboards/index.rb
+++ spec/pages/dashboards/index.rb
@@ -4,10 +4,6 @@ module Pages
   module Dashboards
     class Index < SitePrism::Page
       set_url "/dashboards"
-
-      element :email_field, "#new_player input[type=email]"
-      element :password_field, "#new_player input[type=password]"
-      element :login_button, "#new_player input[type=submit]"
     end
   end
 end
